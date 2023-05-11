e3063c30a8fbd5a3990a82aa13350b94829a3c61
DylanCaff <dylancaff0902@gmail.com>
2023-05-11 11:19:22 -0400

    Update thoughtRoutes.js

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+const { get } = require('mongoose');
 const {
     getAllThoughts,
     createThought,
@@ -9,4 +10,26 @@ const {
     createReaction,
     removeReaction,
 } = require('../../controllers/thoughtController');
+
+router.route('/').get(getAllThoughts).post(createThought);
+
+
+router
+    .route('/:id')
+    .get(getThroughtID)
+    .put(updateThoughtByID)
+    .delete(deleteThoughtByID);
+
+
+router
+    .routes('/:thoughtId/reactions')
+    .post(createReaction)
+    .delete(removeReaction);
+
+
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
+
+
+module.exports = router;
 
